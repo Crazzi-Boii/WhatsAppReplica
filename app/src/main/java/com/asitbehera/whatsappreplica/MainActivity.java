5930fadf48434445e2d988aff33b1d7afabfefb1
Crazzi_Boii <cool.aryo@gmail.com>
2018-03-30 17:36:52 +0530

    Initial commit

---
+++ app/src/main/java/com/asitbehera/whatsappreplica/MainActivity.java
@@ -0,0 +1,26 @@
+package com.asitbehera.whatsappreplica;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void status(View view){
+
+    }
+    public void chats(View view){
+
+    }
+    public void calls(View view){
+
+    }
+    public void cam(View view){
+
+    }
+}
